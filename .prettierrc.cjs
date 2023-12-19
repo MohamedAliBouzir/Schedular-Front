f929a45f63fab080bb1bf4590ba96eea2506f0e3
MohamedAliBouzir <bzr.medali@gmail.com>
2023-12-19 15:06:20 +0100

    added vite, eslint and prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+const config = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+	  jsxSingleQuote: true,
+	  bracketSameLine: true,
+  };
+  
+  module.exports = config;
