d3bd0916c15ed2dac04856119590a34281cbfffd
MohamedAliBouzir <bzr.medali@gmail.com>
2024-01-08 14:29:56 +0100

    tuning

--- src/hooks/SWR/useUser.ts
+++ src/hooks/SWR/useUser.ts
@@ -1,15 +1,14 @@
 import { useContext } from 'react';
-import Cookies from 'js-cookie';
 import authContext from '../../contexts/authContext';
 import { fetcher } from '../../helpers/helpers';
 
 const useUser = () => {
-  const { setUser, user } = useContext(authContext);
+  const { setUser, user, authToken } = useContext(authContext);
   const getUser = async () => {
-    const data = await fetcher('http://localhost:3333/users/me', {
+    const data = await fetcher(`${import.meta.env.VITE_BACK_URL}/users/me`, {
       method: 'GET',
       credentials: 'include',
-      headers: { 'Content-type': 'application/json', Authorization: Cookies.get('authToken')! },
+      headers: { 'Content-type': 'application/json', Authorization: authToken! },
     });
     if (data) {
       setUser(data);
@@ -17,7 +16,7 @@ const useUser = () => {
   };
   const removeUser = () => {
     setUser(null);
-  }
+  };
   return { getUser, removeUser, user };
 };
 export default useUser;
